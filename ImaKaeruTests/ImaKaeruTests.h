c0fec0b453c19b189a3d2d83eeb33e47a031d9d8
Takuya Murakami <tmurakam@tmurakam.org>
2015-01-07 00:13:50 +0900

    OCUnit -> XCTest 移行

--- ImaKaeruTests/ImaKaeruTests.h
+++ ImaKaeruTests/ImaKaeruTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface ImaKaeruTests : SenTestCase
+@interface ImaKaeruTests : XCTestCase
 
 @end
